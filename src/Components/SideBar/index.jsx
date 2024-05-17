ae7c99146eea7ede31dbed4aa3ecf997daa23882
saracampss <saracampxx@gmail.com>
2024-05-17 16:03:23 -0300

    Create Login Page
    Co-authored-by: ingridSCarvalho <ingridcarvalhoisc30@gmail.com>

--- src/Components/SideBar/index.jsx
+++ src/Components/SideBar/index.jsx
@@ -1,9 +1,21 @@
 import ButtonGroup from "@mui/material/ButtonGroup";
 import "./index.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export default function SideBar( { buttons }) {
-
+export default function SideBar({ buttons }) {
+  const theme = createTheme({
+    palette: {
+      custom: {
+        main: "#341F14", // Cor principal personalizada
+        contrastText: "#341F14", // Cor de contraste personalizada
+      },
+    },
+    typography: {
+      fontFamily: '"Noto Sans", "Overpass", sans-serif', // Definindo a fonte padrão
+    },
+  });
+  
   return (
     <div className="side-bar">
       <img
@@ -11,27 +23,33 @@ export default function SideBar( { buttons }) {
         src="src/assets/sindpol-logo.png"
         alt="Sindpol Logo"
       />
-      <p className="sentinela">SENTINELA</p>
+      <img
+        className="sentinela"
+        src="src/assets/sentinela-logo.png"
+        alt="Sentinela Logo"
+      />
       <div className="menu-lateral">
-      <ButtonGroup
-        orientation="vertical"
-        aria-label="Vertical button group"
-        variant="text"
-        sx={{
-          "& .MuiButton-root": {
-            color: "#3D160D", // Cor do texto dos botões
-            borderColor: "#3D160D", // Cor da borda dos botões
-          },
-        }}
-      >
-        { buttons }
-      </ButtonGroup>
+        <ThemeProvider theme={theme}>
+          <ButtonGroup
+            orientation="vertical"
+            aria-label="Vertical button group"
+            variant="text"
+            sx={{
+              width: "380px",
+              "& .MuiButton-root": {
+                color: "#3D160D", // Cor do texto dos botões
+                borderColor: "#3D160D", // Cor da borda dos botões
+              },
+            }}
+          >
+            {buttons}
+          </ButtonGroup>
+        </ThemeProvider>
       </div>
     </div>
   );
 }
 
 SideBar.propTypes = {
-    buttons: PropTypes.array.isRequired,
-  };
-  
+  buttons: PropTypes.array.isRequired,
+};
--- src/Login/index.jsx
+++ src/Login/index.jsx
@@ -1,13 +1,210 @@
-import SideBar from "../Components/SideBar"
+import SideBar from "../Components/SideBar";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function Login() {
+  const theme = createTheme({
+    palette: {
+      custom: {
+        button: "#AE883C", // Cor para o texto e stroke do botão
+        content: "#EAE3D7", // Cor para o conteúdo interno do botão
+        main: "#341F14", // Cor principal personalizada
+        contrastText: "#341F14", // Cor de contraste personalizada
+      },
+    },
+    typography: {
+      fontFamily: '"Noto Sans", "Overpass", sans-serif', // Definindo a fonte padrão
+    },
+  });
+
+  const rgbaValue01 = `rgba(${parseInt(
+    theme.palette.custom.button.slice(-6, -4),
+    16
+  )}, ${parseInt(theme.palette.custom.button.slice(-4, -2), 16)}, ${parseInt(
+    theme.palette.custom.button.slice(-2),
+    16
+  )}, 0.1)`;
+
+  const rgbaValue09 = `rgba(${parseInt(
+    theme.palette.custom.button.slice(-6, -4),
+    16
+  )}, ${parseInt(theme.palette.custom.button.slice(-4, -2), 16)}, ${parseInt(
+    theme.palette.custom.button.slice(-2),
+    16
+  )}, 0.9)`;
+
   const buttons = [
-    <Button key="login">Login</Button>,
-    <Button key="filiacao">Filiação</Button>,
-    <Button key="sobre">Sobre</Button>,
+    <Button
+      key="login"
+      sx={{
+        "&amp;:hover": {
+          backgroundColor: rgbaValue01,
+        },
+      }}
+    >
+      Login
+    </Button>,
+    <Button
+      key="filiacao"
+      sx={{
+        "&amp;:hover": {
+          backgroundColor: rgbaValue01,
+        },
+      }}
+    >
+      Filiação
+    </Button>,
+    <Button
+      key="sobre"
+      sx={{
+        "&amp;:hover": {
+          backgroundColor: rgbaValue01,
+        },
+      }}
+    >
+      Sobre
+    </Button>,
   ];
+
   return (
-    <SideBar buttons={ buttons } />
-  )
+    <div className="screen">
+      <SideBar buttons={buttons} />
+      <div className="area-card">
+        <div className="card">
+          <img
+            className="logo"
+            src="src/assets/sindpol-logo.png"
+            alt="Sindpol Logo"
+          />
+          <img
+            className="sentinela"
+            src="src/assets/sentinela-logo.png"
+            alt="Sentinela Logo"
+          />
+          <ThemeProvider theme={theme}>
+            <TextField
+              id="standard-basic"
+              label="MATRÍCULA"
+              variant="standard"
+              focused
+              placeholder="Digite sua matrícula"
+              sx={{
+                width: "400px",
+                "&amp; .MuiInput-underline:before": {
+                  borderBottomColor: theme.palette.custom.main, // Cor da borda inferior antes do foco
+                },
+                "&amp; .MuiInput-underline:hover:before": {
+                  borderBottomColor: theme.palette.custom.main, // Cor da borda inferior ao passar o mouse
+                },
+                "&amp; .MuiInput-underline:after": {
+                  borderBottomColor: theme.palette.custom.main, // Cor da borda inferior após o foco
+                },
+                "&amp; .MuiInputBase-input": {
+                  color: theme.palette.custom.contrastText, // Cor do texto
+                },
+                "&amp; .MuiInputLabel-root": {
+                  color: theme.palette.custom.main,
+                  fontFamily: '"Noto Sans", sans-serif',
+                },
+                "&amp; .MuiInputLabel-root.Mui-focused": {
+                  color: theme.palette.custom.main, // Cor do rótulo quando focado
+                },
+                "&amp; .MuiInputBase-input::placeholder": {
+                  fontFamily: '"Overpass", sans-serif', // Fonte do placeholder
+                },
+              }}
+            />
+            <TextField
+              id="standard-basic"
+              label="SENHA"
+              variant="standard"
+              focused
+              placeholder="Digite sua senha"
+              sx={{
+                width: "400px",
+                fontfamily: "Noto Sans",
+                marginTop: "33px",
+                "&amp; .MuiInput-underline:before": {
+                  borderBottomColor: theme.palette.custom.main, // Cor da borda inferior antes do foco
+                },
+                "&amp; .MuiInput-underline:hover:before": {
+                  borderBottomColor: theme.palette.custom.main, // Cor da borda inferior ao passar o mouse
+                },
+                "&amp; .MuiInput-underline:after": {
+                  borderBottomColor: theme.palette.custom.main, // Cor da borda inferior após o foco
+                },
+                "&amp; .MuiInputBase-input": {
+                  color: theme.palette.custom.contrastText, // Cor do texto
+                },
+                "&amp; .MuiInputLabel-root": {
+                  color: theme.palette.custom.main,
+                  fontFamily: '"Noto Sans", sans-serif',
+                },
+                "&amp; .MuiInputLabel-root.Mui-focused": {
+                  color: theme.palette.custom.main, // Cor do rótulo quando focado
+                },
+                "&amp; .MuiInputBase-input::placeholder": {
+                  fontFamily: '"Overpass", sans-serif', // Fonte do placeholder
+                },
+              }}
+            />
+            <div className="recupera-senha">
+              <Button
+                key="senha"
+                variant="text"
+                sx={{
+                  textTransform: "none",
+                  textDecoration: "underline",
+
+                  color: theme.palette.custom.main, // Cor do texto do botão
+                  "&amp;:hover": {
+                    transform: "scale(1.03)",
+                    backgroundColor: rgbaValue01,
+                  },
+                }}
+              >
+                Esqueci a senha
+              </Button>
+            </div>
+            <Button
+              variant="contained"
+              sx={{
+                fontFamily: "Noto Sans, sans-serif", // Definindo a fonte para Noto Sans Display Medium
+                color: theme.palette.custom.button, // Cor do texto do botão
+                backgroundColor: "transparent", // Fundo transparente
+                border: `2px solid ${theme.palette.custom.button}`, // Cor do stroke em torno do botão
+                width: "400px", // Largura do botão
+                marginTop: "50px",
+                "&amp;:hover": {
+                  transform: "scale(1.03)",
+                  backgroundColor: rgbaValue01,
+                },
+              }}
+            >
+              Filiar-me ao sindicato
+            </Button>
+
+            <Button
+              variant="contained"
+              sx={{
+                fontFamily: "Noto Sans, sans-serif", // Definindo a fonte para Noto Sans Display Medium
+                color: theme.palette.custom.content, // Cor do texto do botão
+                backgroundColor: theme.palette.custom.button, // Fundo transparente
+                width: "400px", // Largura do botão
+                marginTop: "15px",
+                "&amp;:hover": {
+                  transform: "scale(1.03)",
+                  backgroundColor: rgbaValue09,
+                },
+              }}
+            >
+              Entrar
+            </Button>
+          </ThemeProvider>
+        </div>
+      </div>
+    </div>
+  );
 }
