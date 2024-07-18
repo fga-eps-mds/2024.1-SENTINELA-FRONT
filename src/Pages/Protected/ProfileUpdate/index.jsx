92bc624eedda8754ede86f1bf2d85248428a5fe6
Jose Eduardo <jevrprado@gmail.com>
2024-07-18 12:08:55 -0300

    [fix:2] - add user view title and personal data section
    Co-authored-by: Diogo Barboza <diogorodriguesbb@gmail.com>
    Signed-off-by: Jose Eduardo <jevrprado@gmail.com>

--- src/Pages/Protected/ProfileUpdate/index.jsx
+++ src/Pages/Protected/ProfileUpdate/index.jsx
@@ -1,6 +1,7 @@
 import SideBar from "../../../Components/SideBar";
 import React from "react";
 import SideButton from "../../../Components/SideButton";
+import LabeledTextField from "../../../Components/LabeledTextField";
 
 const ProfileUpdate = () => {
 
@@ -9,10 +10,15 @@ const ProfileUpdate = () => {
         <SideButton key="filiacao" text="Cadastro" />
       ];
 
-    return(
-        <SideBar buttons={buttons}/>
-
-    );
+      return (
+        <div style={{ display: "flex" }}>
+          <SideBar buttons={buttons} />
+          <div style={{ flex: 1, padding: "20px" }}>
+          <h1 style={{marginTop: "100px", marginLeft: "30px"}}>Visualização de Usuário</h1>
+          <h3 style={{marginTop: "35px", marginLeft: "65px", fontSize: "20px" }}>Dados Pessoais</h3>
+          </div>
+        </div>
+      );
 };
 
 export default ProfileUpdate;
