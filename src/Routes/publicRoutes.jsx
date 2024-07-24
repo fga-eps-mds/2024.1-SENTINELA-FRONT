8170b25841a9b1cc489144dd1447d89ba0a36763
Anna Clara Cardoso Evangelista Brandão <annacebrandao@gmail.com>
2024-07-24 15:38:32 -0300

    [fix:18] - Movendo pastas ListSupplier, ViewSupplier e Supplier pra protected
    Co-authored-by: @davimesquit <davimesquita.2004@gmail.com>
    Signed-off-by: Anna Clara Cardoso Evangelista Brandão <annacebrandao@gmail.com>

--- src/Routes/publicRoutes.jsx
+++ src/Routes/publicRoutes.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider, BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from '../Pages/Public/Login';
 import Home from '../Pages/Protected/Home';
-import Supplier from '../Pages/Public/Supplier';
-import ViewSupplier from '../Pages/Public/ViewSupplier';
-import ListSupplier from '../Pages/Public/ListSupplier';
+//import Supplier from '../Pages/Public/Supplier';
+//import ViewSupplier from '../Pages/Public/ViewSupplier';
+//import ListSupplier from '../Pages/Public/ListSupplier';
 
 const PublicRoutes = () => {
   return (
@@ -12,9 +12,6 @@ const PublicRoutes = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/supplier" element={<Supplier />} />
-        <Route path="/listsupplier" element={<ListSupplier />} />
-        <Route path="/viewsupplier" element={<ViewSupplier />} />
       </Routes>
     </BrowserRouter>
   );
