cc1348839dc44335e8c6b7ac66cab325fb2b8eb2
AlGouvea <alvarohsgouvea@gmail.com>
2024-07-24 21:50:18 -0300

    [fix] Remoção de código comentado inutilizado

--- src/Services/BaseService/index.js
+++ src/Services/BaseService/index.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
 const baseUserURL = "http://localhost:3001/";
-// const baseBankURL = "...";
 
 const APIUsers = axios.create({
   baseURL: baseUserURL,
 });
-
-// const APIBank = axios.create({
-//     baseURL: baseUserURL,
-// });
 
 export { APIUsers };
