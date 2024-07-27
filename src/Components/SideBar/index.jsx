9d510cc9f01e8574dcd5d63571839426c16a56a3
storch7 <guilhermestorch7@gmail.com>
2024-07-27 16:59:30 -0300

    [feat:20] midificação na importação de imagens da sidebar
    Co-authored-by: @MilenaFRocha <milenafernandes594@gmail.com>

--- src/Components/SideBar/index.jsx
+++ src/Components/SideBar/index.jsx
@@ -1,18 +1,20 @@
 import "./index.css";
 import PropTypes from "prop-types";
 import { ButtonGroup } from "@mui/material";
+import sindpolLogo from "../../assets/sindpol-logo.png";
+import sentinelaLogo from "../../assets/sentinela-logo.png"
 
 export default function SideBar({ buttons }) {
   return (
     <div className="side-bar">
       <img
         className="logo"
-        src="src/assets/sindpol-logo.png"
+        src={sindpolLogo}
         alt="Sindpol Logo"
       />
       <img
         className="sentinela"
-        src="src/assets/sentinela-logo.png"
+        src={sentinelaLogo}
         alt="Sentinela Logo"
       />
       <div className="menu-lateral">
