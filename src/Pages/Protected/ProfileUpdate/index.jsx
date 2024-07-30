45788baf6581a9dfbf9ccbfbffc433386f720165
yukioz <yukio.link@gmail.com>
2024-07-30 15:41:43 -0300

    [fix:18] - fix eslint errors.
    Signed-off-by: yukioz <yukio.link@gmail.com>

--- src/Pages/Protected/ProfileUpdate/index.jsx
+++ src/Pages/Protected/ProfileUpdate/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import FieldText from "../../../Components/FieldText";
 import FieldNumber from "../../../Components/FieldNumber";
