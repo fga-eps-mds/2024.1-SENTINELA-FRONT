ba80c76dc141454615c3fec2837d19c7e95a2e1f
storch7 <guilhermestorch7@gmail.com>
2024-08-01 00:07:35 -0300

    [feat:52] Criação da finção de renderização da página
    Co-Authored-by:  @MilenaFRocha <milenafernandes594@gmail.com>

---
+++ src/Pages/Protected/MembershipRequests/index.jsx
@@ -0,0 +1,13 @@
+import "./index.css"
+import { useAuth } from "../../../Context/auth";
+
+export default function MembershipRequest() {
+    const { user } = useAuth();
+    
+    return user && (
+        <section>
+            Página de listagem de solicitação de filiação
+        </section>
+
+    )
+}
