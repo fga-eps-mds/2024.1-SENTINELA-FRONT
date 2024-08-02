36d6f05dc215744c22885a71cf77e7f959e9c41a
Davi Mesquita <davimesquita.2004@gmail.com>
2024-08-02 13:51:43 -0300

    [feat:17] - Terminando ListRoles >> >> >> >> >> >> Co-authored-by: Jagaima <davinobre.ik@gmail.com> >> >> Co-authored-by: Gam13 <joaovictorgam13@gmail.com>
    Signed-off-by: Davi Mesquita <davimesquita.2004@gmail.com>

--- src/Pages/Protected/Roles/RolesListPage/index.jsx
+++ src/Pages/Protected/Roles/RolesListPage/index.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./index.css";
+import "../../Users/userListPage/index.css";
 import PrimaryButton from "../../../../Components/PrimaryButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
 import ListItemText from "@mui/material/ListItemText";
-import SecondaryButton from "../../../../Components/SecondaryButton";
 import FieldText from "../../../../Components/FieldText";
 import { APIUsers } from "../../../../Services/BaseService";
 
@@ -16,12 +15,13 @@ export default function RolesListPage() {
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
 
-  const storagedUser = localStorage.getItem("@App:user");
-
   useEffect(() => {
     const fetchRoleForm = async () => {
       try {
-        const response = await APIUsers.get(`/role`, {
+        const storagedUserString = localStorage.getItem("@App:user");
+        const storagedUser = JSON.parse(storagedUserString);
+
+        const response = await APIUsers.get("role", {
           headers: {
             Authorization: `Bearer ${storagedUser.token}`,
           },
@@ -46,7 +46,7 @@ export default function RolesListPage() {
   };
 
   const handleItemClick = (role) => {
-    navigate("/perfis/${role.nome}", {
+    navigate(`/perfis/editar/${role.name}`, {
       state: { roleId: role._id },
     });
   };
@@ -55,46 +55,49 @@ export default function RolesListPage() {
     role.name.toLowerCase().includes(search.toLowerCase())
   );
 
-  <section className="container">
-    <div className="forms-container">
-      <div className="double-box">
-        <h1>Lista de perfis</h1>
+  return (
+    <section className="container">
+      <div className="forms-container">
+        <div className="double-box">
+          <h1>Lista de perfis</h1>
+        </div>
+
+        <div className="search-box">
+          <FieldText
+            label="Pesquisar perfil"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <PrimaryButton text="Cadastrar perfil" onClick={handleSubmit} />
+        </div>
+
+        <List>
+          {filteredRoles.map((role, index) => (
+            <div key={role._id}>
+              <ListItem>
+                <ListItemButton
+                  className="list-item"
+                  style={{
+                    transition: "background-color 0.3s ease",
+                  }}
+                  onMouseEnter={(e) =>
+                    (e.currentTarget.style.backgroundColor =
+                      "rgba(0, 0, 0, 0.1)")
+                  }
+                  onMouseLeave={(e) =>
+                    (e.currentTarget.style.backgroundColor = "transparent")
+                  }
+                  onClick={() => handleItemClick(role)}
+                >
+                  <ListItemText primary={role.name} />
+                </ListItemButton>
+              </ListItem>
+
+              {index < filteredRoles.length - 1 && <Divider />}
+            </div>
+          ))}
+        </List>
       </div>
-
-      <div className="search-box">
-        <FieldText
-          label="Pesquisar perfil"
-          value="search"
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <PrimaryButton text="Cadastrar usuário" onClick={handleSubmit} />
-      </div>
-
-      <List>
-        {filteredRoles.map((role, index) => (
-          <div key={role._id}>
-            <ListItem>
-              <ListItemButton
-                className="list-item"
-                style={{
-                  transition: "background-color 0.3s ease",
-                }}
-                onMouseEnter={(e) =>
-                  (e.currentTarget.style.backgroundColor = "rgba(0, 0, 0, 0.1)")
-                }
-                onMouseLeave={(e) =>
-                  (e.currentTarget.style.backgroundColor = "transparent")
-                }
-                onClick={() => handleItemClick(role)}
-              >
-                <ListItemText primary={role.nome} />
-              </ListItemButton>
-            </ListItem>
-
-            {index < filteredRoles.length - 1 && <Divider />}
-          </div>
-        ))}
-      </List>
-    </div>
-  </section>
+    </section>
+  );
 }
