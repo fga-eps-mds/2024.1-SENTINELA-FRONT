d888ee14fc2302644911eaf45551fcbb361647c2
Milena Linda <milenafernandes594@gmail.com>
2024-08-06 17:06:30 -0300

    [fix:52] - Bug corrigido
    Co-Authored-by: @storch7 <guilhermestorch7@gmail.com
    Signed-off-by: Milena Linda <milenafernandes594@gmail.com>

--- src/Pages/Protected/MembershipRequests/index.jsx
+++ src/Pages/Protected/MembershipRequests/index.jsx
@@ -20,9 +20,10 @@ export default function MembershipRequest() {
   const [search, setSearch] = useState("");
   const [isResultReadOnly, setIsResultReadOnly] = useState(false);
   const [checkedItems, setCheckedItems] = useState([]);
-  const [openSucessAproval, setSucesssAproval] = useState(false);
-  const [openSucessDelete, setSucessDelete] = useState(false);
+  const [openSuccessApproval, setSuccessApproval] = useState(false);
+  const [openSuccessDelete, setSuccessDelete] = useState(false);
   const [openTryingDelete, setTryingDelete] = useState(false);
+  const [noResultsFound, setNoResultsFound] = useState(false);
   const navigate = useNavigate();
 
   // Fetch all members on component mount
@@ -41,35 +42,32 @@ export default function MembershipRequest() {
       }
     }
     fetchData();
-  });
+  }, []);
 
   const handleSearch = () => {
     // Filter members based on the search query
-    const filteredData = members.filter((member) =>
+    const filteredData = members.filter(member =>
       member.nomeCompleto.toLowerCase().includes(search.toLowerCase())
     );
     setFilteredMembers(filteredData);
+    setNoResultsFound(filteredData.length === 0); // Update state based on results
     setIsResultReadOnly(true); // Make this field read-only after search
   };
 
   const handleCheckboxChange = (newChecked) => {
-    // Update state with the new checked items
     setCheckedItems(newChecked);
   };
 
   const handleConfirm = async () => {
     try {
-      // Filter out members whose names are checked
       const membersToUpdate = filteredMembers.filter((member) =>
         checkedItems.includes(member.nomeCompleto)
       );
 
-      // Update the status of the selected members
       for (const member of membersToUpdate) {
         await updateMemberStatus(member._id);
       }
 
-      // Optionally, re-fetch members or update state to reflect changes
       const result = await getMemberShip();
       if (result.message) {
         console.log(result.message);
@@ -78,14 +76,15 @@ export default function MembershipRequest() {
       setMembers(result);
       setFilteredMembers(result);
       setCheckedItems([]);
-      setSucesssAproval(true);
+      setNoResultsFound(false);
+      setSuccessApproval(true);
     } catch (error) {
       console.error("Error updating member status:", error);
     }
   };
+
   const handleReject = async () => {
     try {
-      // Filter out members whose names are checked
       const membersToDelete = filteredMembers.filter((member) =>
         checkedItems.includes(member.nomeCompleto)
       );
@@ -94,13 +93,12 @@ export default function MembershipRequest() {
       }
 
       setTryingDelete(false);
-      setSucessDelete(true);
+      setSuccessDelete(true);
     } catch (error) {
       console.error("Error deleting member status:", error);
     }
   };
 
-  // Filter members with status = true for the checklist
   const membersForCheckList = filteredMembers.filter((member) => member.status);
 
   return (
@@ -113,12 +111,14 @@ export default function MembershipRequest() {
             type="text"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            readOnly={isResultReadOnly} // Make field read-only after search
+            readOnly={isResultReadOnly}
           />
           <SecondaryButton text="Pesquisar" onClick={handleSearch} />
         </div>
         <div className="member-list">
-          {membersForCheckList.length > 0 ? (
+          {noResultsFound ? (
+            <p>Nenhum membro encontrado.</p>
+          ) : membersForCheckList.length > 0 ? (
             <>
               <CheckList
                 items={membersForCheckList.map((member) => member.nomeCompleto)}
@@ -145,14 +145,14 @@ export default function MembershipRequest() {
         </div>
 
         <Modal
-          show={openSucessAproval}
+          show={openSuccessApproval}
           alertTitle="Solicitações aprovadas"
           alert="Os filiados devem receber em breve um e-mail para gerar a senha de acesso"
           width="100%"
         >
           <PrimaryButton
             text="Fechar"
-            onClick={() => setSucesssAproval(false)}
+            onClick={() => setSuccessApproval(false)}
           />
         </Modal>
 
@@ -163,15 +163,14 @@ export default function MembershipRequest() {
           width="100%"
         >
           <PrimaryButton text="Excluir solicitações" onClick={handleReject} />
-
           <SecondaryButton
-            text="Cancelr e manter solicitações"
+            text="Cancelar e manter solicitações"
             onClick={() => setTryingDelete(false)}
           />
         </Modal>
 
         <Modal
-          show={openSucessDelete}
+          show={openSuccessDelete}
           alertTitle="Solicitações excluídas"
           alert="Os filiados foram excluídos com sucesso"
           width="100%"
