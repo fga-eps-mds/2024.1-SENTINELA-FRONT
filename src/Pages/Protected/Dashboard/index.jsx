5407f2444571b592d8446a4faa77028ddeb2b306
storch7 <guilhermestorch7@gmail.com>
2024-08-08 13:59:54 -0300

    [feat:31] Inserção das configurações iniciais de css e responsividade
    Co-authored-by: Gam13 <joaovictorgam13@@gmail.com>

--- src/Pages/Protected/Dashboard/index.jsx
+++ src/Pages/Protected/Dashboard/index.jsx
@@ -1,5 +1,19 @@
 import "./index.css";
 
 export default function Dashboard() {
-  return <section>THIS IS THE DASHBOARD</section>;
+  return (
+    <section className="dash-section">
+      <div>
+        <h1>Filiados</h1>
+      </div>
+
+      <div>
+        <h1>Divisão de sexo por lotação</h1>
+      </div>
+
+      <div>
+        <h1>Filiações e Desfiliações</h1>
+      </div>
+    </section>
+  );
 }
