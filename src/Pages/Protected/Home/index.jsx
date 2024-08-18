82094081c2bb3b6c899c65ebcbaaf6788d19c21e
Gam13 <joaovictorgam13@gmail.com>
2024-08-18 00:04:47 -0300

    [Feat 31] Starting Filters on userdata
    Co-Authored-by: @storch7 <guilhermestorch7@gmail.com>
    Signed-off-by: Gam13 <joaovictorgam13@gmail.com>

--- src/Pages/Protected/Home/index.jsx
+++ src/Pages/Protected/Home/index.jsx
@@ -1,14 +1,67 @@
+import { useState, useEffect } from "react";
 import { useAuth } from "../../../Context/auth";
+import { getUsers } from "../../../Services/userService";
+import FieldSelect from "../../../Components/FieldSelect";
+import "./index.css";
 
 const Home = () => {
   const { user } = useAuth();
+  const [data, setData] = useState([]);
+  const [isSind, setIsSind] = useState("");
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await getUsers();
+        if (Array.isArray(response)) {
+          setData(response);
+        } else {
+          console.error("Os dados recebidos não são um array.");
+        }
+      } catch (error) {
+        console.error("Erro ao buscar usuários:", error);
+      }
+    };
+
+    fetchUsers();
+  });
 
   return (
     user && (
-      <div>
-        <h1>Home</h1>
-        <div>Bem vindo {user.nomeCompleto}</div>
-      </div>
+      <section className="dash-section">
+        <div>
+          <h1>Filiados</h1>
+          <div className="filiados">
+            <h1>Total {Object.keys(data).length}</h1>
+            <h1>
+              {isSind}{" "}
+              {isSind == "Sindicalizado"
+                ? data.filter((item) => item.status === true).length
+                : data.filter((item) => item.status === false).length}
+            </h1>
+          </div>
+          <FieldSelect
+            label="Filtro"
+            onChange={(e) => {
+              setIsSind(e.target.value);
+            }}
+            options={["Sindicalizado", "Não Sindicalizado"]}
+            value={isSind}
+          />
+        </div>
+
+        <div>
+          <h1>Divisão de sexo por lotação</h1>
+        </div>
+
+        <div>
+          <h1>Filiações e Desfiliações</h1>
+        </div>
+
+        <div>
+          <h1>Filiações e Desfiliações</h1>
+        </div>
+      </section>
     )
   );
 };
