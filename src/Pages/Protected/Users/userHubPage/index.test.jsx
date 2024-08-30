9faeca2a92ff647c7b7ccf804c28289152d24274
saracampss <saracampxx@gmail.com>
2024-08-30 16:22:51 -0300

    [fix:34] - fix benefits tests
    Signed-off-by: saracampss <saracampxx@gmail.com>

--- src/Pages/Protected/Users/userHubPage/index.test.jsx
+++ src/Pages/Protected/Users/userHubPage/index.test.jsx
@@ -16,38 +16,15 @@ describe("UserHubPage", () => {
 
   it("renders all buttons correctly", () => {
     expect(screen.getByText("Filiações pendentes")).toBeInTheDocument();
-    expect(screen.getByText("CADASTRO DE USUÁRIOS")).toBeInTheDocument();
     expect(screen.getByText("LISTA DE USUÁRIOS")).toBeInTheDocument();
-    expect(screen.getByText("ATUALIZAR USUÁRIO")).toBeInTheDocument();
-    expect(screen.getByText("CADASTRO DE PERFIL")).toBeInTheDocument();
     expect(screen.getByText("LISTA DE PERFIL")).toBeInTheDocument();
-  });
-
-  it("navigates to cadastro de usuarios page on button click", async () => {
-    fireEvent.click(screen.getByText(/CADASTRO DE USUÁRIOS/i));
-    await waitFor(() => {
-      expect(window.location.pathname).toBe("/usuarios/criar");
-    });
+    expect(screen.getByText("LISTA DE ÓRGÃOS")).toBeInTheDocument();
   });
 
   it("navigates to lista de usuarios page on button click", async () => {
     fireEvent.click(screen.getByText(/LISTA DE USUÁRIOS/i));
     await waitFor(() => {
       expect(window.location.pathname).toBe("/usuarios");
-    });
-  });
-
-  it("navigates to atualizar dados page on button click", async () => {
-    fireEvent.click(screen.getByText(/ATUALIZAR USUÁRIO/i));
-    await waitFor(() => {
-      expect(window.location.pathname).toBe("/perfil");
-    });
-  });
-
-  it("navigates to cadastro de perfil page on button click", async () => {
-    fireEvent.click(screen.getByText(/CADASTRO DE PERFIL/i));
-    await waitFor(() => {
-      expect(window.location.pathname).toBe("/perfis/criar");
     });
   });
 
