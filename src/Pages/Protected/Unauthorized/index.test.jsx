91de9bab50ec5cf6410b0cf759a921dfae73d614
Gam13 <joaovictorgam13@gmail.com>
2024-08-31 22:22:15 -0300

    [test 33] unauthorized page test
    Co-Authored-by: Diogo-Barboza <diogorodriguesbb@gmail.com>

--- src/Pages/Protected/Unauthorized/index.test.jsx
+++ src/Pages/Protected/Unauthorized/index.test.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+// src/Pages/Protected/Unauthorized/index.test.jsx
+import { describe, it, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Unauthorized from "./index";
+import "@testing-library/jest-dom";
 
 describe("Unauthorized Page", () => {
-  it("should render the Unauthorized page with correct content", () => {
-    render(<Unauthorized />);
+  beforeEach(() => {
+    // Qualquer configuração necessária antes de cada teste
+  });
 
-    const titleElement = screen.getByRole("heading", {
-      name: /acesso negado/i,
-    });
+  it("renders the Unauthorized page with correct content", () => {
+    // Renderiza o componente Unauthorized dentro de um Router
+    render(
+      <Router>
+        <Unauthorized />
+      </Router>
+    );
+
+    // Verifica se o título "Acesso Negado" está na tela
+    const titleElement = screen.getByRole("heading", { name: /acesso negado/i });
     expect(titleElement).toBeInTheDocument();
 
-    const paragraphElement = screen.getByText(
-      /você não tem permissão para acessar esta página/i
-    );
+    // Verifica se o parágrafo de permissão está na tela
+    const paragraphElement = screen.getByText(/você não tem permissão para acessar esta página/i);
     expect(paragraphElement).toBeInTheDocument();
   });
 });
