75554e1d7c6de893de3fc88f9b5ba889910e853b
Gam13 <joaovictorgam13@gmail.com>
2024-09-02 22:37:32 -0300

    lint

--- src/Pages/Protected/Benefit/Benefits/index.test.jsx
+++ src/Pages/Protected/Benefit/Benefits/index.test.jsx
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Benefits from "./index";
