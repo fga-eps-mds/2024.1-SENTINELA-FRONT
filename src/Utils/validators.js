00cbd7b8a9a4ce5508478d56089f28de27b249a3
ingridSCarvalho <ingridCarvalhoisc30@gmail.com>
2024-09-08 10:07:39 -0300

    [fix] - mascara de cnpj

--- src/Utils/validators.js
+++ src/Utils/validators.js
@@ -94,8 +94,8 @@ export const handleCpfCnpjInput = (value) => {
       .replace(/^(\d{2})(\d)/, "$1.$2")
       .replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
       .replace(/\.(\d{3})(\d)/, ".$1/$2")
-      .replace(/(\d{4})(\d{1,2})$/, "$1-$2")
-      .slice(0, 18); // CNPJ formatado
+      .replace(/(\d{4})(\d{1,3})$/, "$1-$2")
+      .slice(0, 18);
   }
 };
 
